041584dbad2849ff4c453f619bae2c87b3a12403
Dmitriy Nemykin <ddraudred@gmail.com>
2020-02-14 11:05:15 +0300

    Network examples

---
+++ network/udp_server.rb
@@ -0,0 +1,18 @@
+require 'socket'
+
+port = ARGV[0]
+
+ds = UDPSocket.new
+ds.bind(nil, port)
+
+loop do
+  request, address = ds.recvfrom(1024)
+  response = request.upcase
+
+  clientaddr = address[3] # client's ip
+  clientname = address[2] # hostname
+  clientport = address[1]
+
+  ds.send(response, 0, clientaddr, clientport)
+  puts "Connected from #{clientname}:#{clientport}"
+end
